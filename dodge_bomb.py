5d955eaea4950b71ed0d94f74c2e2d51f970a410
kazuki <c0c240600f@edu.teu.ac.jp>
2026-04-21 14:49:53 +0900

    練習問題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,6 +16,20 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectか爆弾Rect
+    戻り値：タプル（横方向判定結果, 縦方向判定結果）
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -31,7 +45,6 @@ def main():
     bb_rct.centerx = random.randint(0, WIDTH)  # 初期座標の設定
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5  # 爆弾の速度
-
 
     clock = pg.time.Clock()
     tmr = 0
@@ -58,9 +71,17 @@ def main():
                 sum_mv[1] += mv[1]  #縦方向
     
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  # 位置の判定
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)  # 爆弾を移動させる
+        yoko, tate = check_bound(bb_rct)  # 爆弾の反射
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)  # 爆弾の表示
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
