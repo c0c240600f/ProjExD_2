9adf8a30b5a23bb20012b05c7b4b961af55d085c
kazuki <c0c240600f@edu.teu.ac.jp>
2026-04-21 14:23:39 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,14 +1,15 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP : (0, -5),  #上
-    pg.K_DOWN : (0, +5),  #下
-    pg.K_LEFT : (-5, 0),  #左
-    pg.K_RIGHT : (+5, 0),  #右
+    pg.K_UP : (0, -5),   # 上
+    pg.K_DOWN : (0, +5),  # 下
+    pg.K_LEFT : (-5, 0),  # 左
+    pg.K_RIGHT : (+5, 0),  # 右
 }
 
 
@@ -22,6 +23,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()  # 爆弾のbb_rctの取得
+    bb_rct.centerx = random.randint(0, WIDTH)  # 初期座標の設定
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 爆弾の速度
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -48,6 +59,8 @@ def main():
     
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾を移動させる
+        screen.blit(bb_img, bb_rct)  # 爆弾の表示
         pg.display.update()
         tmr += 1
         clock.tick(50)
